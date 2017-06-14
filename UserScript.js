3e6a0babf2a971963bc07fa40ca5fc66e486301e
MittYa <mittya@foxmail.com>
2017-06-14 22:53:01 +0800

    Init

---
+++ UserScript.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Clean new tab
+// @namespace    CleanNewTab
+// @version      0.1
+// @description  Remove the most visited list, when you create a new tab in Chrome.
+// @author       mittya
+// @match        https://www.google.com/_/chrome/newtab*
+// @grant        GM_addStyle
+// ==/UserScript==
+
+(function() {
+  GM_addStyle('body{min-height:400px !important;} #most-visited{display:none !important;}');
+})();
