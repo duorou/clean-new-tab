45161c701983ed8afda210c91a0d89b78ec92e48
MittYa <mittya@foxmail.com>
2017-06-27 22:24:51 +0800

    Update url rule

--- UserScript.js
+++ UserScript.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         clean new tab [Chrome Only]
 // @namespace    CleanNewTab
-// @version      0.1
+// @version      0.2
 // @description  Remove the most visited list, when you create a new tab in Chrome.
 // @author       mittya
-// @match        https://www.google.com/_/chrome/newtab*
+// @include      https://www.google.*/_/chrome/newtab*
 // @grant        GM_addStyle
 // ==/UserScript==
 
